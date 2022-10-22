8155c8309ad16b110c27cb4520cc5a3518e6e279
GeorgievaP <you@example.com>
2022-10-22 12:39:14 +0300

    TestFirstCommit

---
+++ GitTestGeorgievaP/GitTestGeorgievaP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestGeorgievaP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
